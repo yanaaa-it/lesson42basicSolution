e35625ba15f776d3a516724b825a0aa2ade1f7d5
st <st@A510PC3>
2026-02-18 18:33:19 +0300

    new

---
+++ task01/main.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#define DEFAULTE_SIZE 100
+using namespace std;
+
+int main() {
+	int array[DEFAULTE_SIZE];
+	int length;
+
+	do {
+		system("cls");
+		cout << "input size of number:";
+		cin >> length;
+	} while (length <= 0);
+
+	cout << "input your array elements:";
+	for (int i = 0; i < length; i++)
+	{
+		cin >> array[i];
+	}
+	int n = length / 2;
+	for (int index = 0; index < n; index++)
+	{
+		int temp = array[index];
+		array[index] = array[length - 1 - index];
+		array[length - 1 - index] = temp;
+	}
+	cout << "result array:";
+	for (int i = 0; i < length; i++) {
+		cout << array[i] << " ";
+	}
+	return 0;
+}
