91b7a6e95019f351e49df0094d68d83aacfbef2f
yanaaa-it <yaninalikemiki@gmail.com>
2026-02-21 19:08:58 +0300

    Update

--- task04/logic.h
+++ task04/logic.h
@@ -1,1 +1,10 @@
 #pragma once
+#include<iostream>
+#include<string>
+#define DEFAULT_SIZE 20
+using namespace std;
+
+int max(int array[DEFAULT_SIZE], int length);
+int min(int array[DEFAULT_SIZE], int length);
+double arithmetial_average(int array[DEFAULT_SIZE], int length);
+double geometrical_average(int array[DEFAULT_SIZE], int length);
